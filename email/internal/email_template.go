fb3d046f29b133d8fcc643aa512bd03c54167e4f
Fernando Tapia Rico <fertapric@gmail.com>
2019-07-24 23:16:35 +0200

    Change template

--- email/internal/email_template.go
+++ email/internal/email_template.go
@@ -1,9 +1,11 @@
 package internal
 
 const DefaultSubjectTemplate = `
-{{.CheckSuite.App.Name}} check for {{.Repo.FullName}}#{{.CheckSuite.HeadBranch}} {{.CheckSuite.Status}}
+Build failed for {{.Repo.FullName}}#{{.CheckSuite.HeadBranch}}
 `
 
 const DefauleEmailMarkdownTemplate = `
-Check run [{{ .CheckRun.Name }}]({{ .CheckRun.HTMLURL }}) {{ .CheckRun.Status }} in {{ .Duration }}
+Build results available in {{ .CheckRun.HTMLURL }}
+
+It took {{ .Duration }}
 `
