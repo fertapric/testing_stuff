fc65f8fe49f17af32c10d86bb310d990672202fd
Fernando Tapia Rico <fertapric@gmail.com>
2019-07-24 23:46:39 +0200

    Beautify email

--- email/internal/email_template.go
+++ email/internal/email_template.go
@@ -1,9 +1,19 @@
 package internal
 
 const DefaultSubjectTemplate = `
-{{.CheckSuite.App.Name}} check for {{.Repo.FullName}}#{{.CheckSuite.HeadBranch}}
+Elixir CI: Build failed for {{.Repo.FullName}} ({{.CheckSuite.HeadBranch}})
 `
 
 const DefauleEmailMarkdownTemplate = `
+<p>
 Check run [{{ .CheckRun.Name }}]({{ .CheckRun.HTMLURL }}) {{ .CheckRun.Status }} in {{ .Duration }}
+</p>
+
+<p style="font-size:small;-webkit-text-size-adjust:none;color:#666;">
+  &mdash;
+  <br />
+  You are receiving this because you are the author of the commit.
+  <br />
+  <a href="{{ .CheckRun.HTMLURL }}">View it on GitHub</a>
+</p>
 `
